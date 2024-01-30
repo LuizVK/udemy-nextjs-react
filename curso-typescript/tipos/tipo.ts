144c9c76918df8df454ab0726116bf76e38fd0dc
Luiz Vitor <luizv.karpinski@unochapeco.edu.br>
2024-01-30 20:21:44 -0300

    Capítulo de revisão de TypeScript

---
+++ curso-typescript/tipos/tipo.ts
@@ -0,0 +1,63 @@
+//String
+// O tipo da variável é inferida quando não é definido explicitamente o seu tipo e a mesma for inicializada. Neste caso como string...
+let nome = 'João' 
+console.log(nome)
+// nome = 28
+
+//numbers
+let idade = 27
+// idade = 'Ana'
+idade = 27.5
+console.log(idade)
+
+//boolean
+let possuiHobbies = false
+// possuiHobbies = 1
+console.log(possuiHobbies)
+
+// tipos explícitos
+// Quando não é informado o tipo da variável nem inicializada, a mesma será considerada dinamica (any), como é no js
+let minhaIdade: number 
+minhaIdade = 27
+console.log(typeof minhaIdade)
+// minhaIdade = 'idade é 27'
+// console.log(typeof minhaIdade)
+
+// array
+let hobbies: any[] = ["Cozinhar", "Praticar Esportes"]
+console.log(hobbies[0])
+console.log(typeof hobbies)
+
+hobbies = [100, 200, 300]
+// hobbies = 100
+console.log(hobbies)
+
+// tuplas
+let endereco: [string, number, string] = ["Av Principal", 99, "Bloco A"]
+console.log(endereco)
+
+endereco = ["Rua Importante", 1260, "Bloco C"]
+console.log(endereco)
+
+// enums
+enum Cor {
+    Cinza, // 0 
+    Verde = 100, 
+    Azul = 10,
+    Laranja,
+    Amarelo,
+    Vermelho = 100
+}
+
+let minhaCor: Cor = Cor.Verde
+console.log(minhaCor)
+
+console.log(Cor.Azul)
+console.log(Cor.Laranja, Cor.Amarelo)
+console.log(Cor.Verde, Cor.Vermelho)
+
+// any
+let carro: any = "BMW"
+console.log(carro)
+carro = { marca: "BMW", ano: 2019 }
+console.log(carro)
